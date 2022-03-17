f3f0f8925252769aed0401dc9c8d16ca4ade0bc8
Snoozy <snoozy1995@gmail.com>
2022-03-17 18:57:58 +0100

    Fix

--- backend/src/infrastructure/typeORM/user.schema.ts
+++ backend/src/infrastructure/typeORM/user.schema.ts
@@ -24,7 +24,7 @@ export const UserSchema = new EntitySchema<User>({
   relations: {
     friends: {
       type: 'many-to-many',
-      target: User,
+      target: 'User',
       joinColumn: {
         name: 'uuid',
       },
